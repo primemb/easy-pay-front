4739f6483e60ccb19d85f2f5ad4a252d3fc33c30
Prime <mo.behzad34@gmail.com>
2023-09-07 14:53:05 +0330

    finished verify page

--- src/app/pages/verify/verify.component.ts
+++ src/app/pages/verify/verify.component.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-verify',
   standalone: true,
   templateUrl: './verify.component.html',
   styleUrls: ['./verify.component.css'],
+  imports: [CommonModule],
 })
-export class VerifyComponent {}
+export class VerifyComponent implements OnInit {
+  status: boolean;
+  uniqueId: string;
+  amount: number;
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.status = params['status'] === 'true' ? true : false;
+      this.uniqueId = params['uniqueId'];
+      this.amount = Number(params['amount']);
+    });
+  }
+
+  statusComputedClass() {
+    return this.status
+      ? 'bg-green-100 text-green-700'
+      : 'bg-rose-100 text-rose-700';
+  }
+
+  statusComputedText() {
+    return this.status ? 'تراکنش موفقیت آمیز بود' : 'تراکنش ناموفق بود';
+  }
+
+  back() {
+    this.router.navigate(['/']);
+  }
+}
